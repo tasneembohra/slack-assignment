323c4e12d452118eb5295c2a83c67d2e75eb32fa
tasneembohra <tasneem.bohra@hotmail.com>
2022-09-04 22:33:50 -0600

    Reverse keyword search db query

--- app/src/main/java/com/tasneembohra/slackassignment/repo/db/dao/UserSearchDao.kt
+++ app/src/main/java/com/tasneembohra/slackassignment/repo/db/dao/UserSearchDao.kt
@@ -13,7 +13,7 @@ interface UserSearchDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addDeniedKeyword(keyword: DeniedKeyword)
 
-    @Query("SELECT :keyword FROM denied_keywords WHERE keyword LIKE :keyword || '%' LIMIT 1")
+    @Query("SELECT :keyword FROM denied_keywords WHERE :keyword LIKE keyword || '%' LIMIT 1")
     fun getDeniedKeyword(keyword: String): String?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
